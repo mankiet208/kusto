930f2cb5d59f61914b292da7936b018ecf5c4618
Kiet Truong <kiettruong@bbv.ch>
2024-03-22 17:40:40 +0700

    Add logger function

--- Kusto/Helper/Logger.swift
+++ Kusto/Helper/Logger.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+enum LogType: String{
+    case error
+    case warning
+    case success
+    case debug
+}
+
+
+class Logger {
+    
+    static func log(_ logType: LogType, _ message: String) {
+        switch logType {
+        case .error:
+            print("\n🔥 Error: \(message)\n")
+        case .warning:
+            print("\n⚠️ Warning: \(message)\n")
+        case .success:
+            print("\n✅ Success: \(message)\n")
+        case .debug:
+            print("\nℹ️ Debug: \(message)\n")
+        }
+    }
+}
